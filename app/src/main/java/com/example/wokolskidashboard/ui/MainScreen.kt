537fcf30fcf34aa4602ae5ecfd02b244cf75c3c9
Xlayync <igor.mejsner@cyfroweszkoly.pl>
2026-04-28 08:19:52 +0200

    Calculate balance

--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,2 +1,15 @@
 package com.example.wokolskidashboard.ui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import com.example.wokolskidashboard.model.Transaction
+
+@Composable
+fun MainScreen(){
+    val transactions = remember { mutableStateListOf<Transaction>() }
+
+    val balance = transactions.sumOf {
+        if (it.isExpense) -it.amount else it.amount
+    }
+}
